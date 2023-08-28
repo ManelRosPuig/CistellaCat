f5345ab9233537f26f5583c097d0940136d72ee4
ManelRosPuig <manel.rospuig@gmail.com>
2023-08-28 18:39:31 +0200

    Add General Provider and counter test

--- lib/widgets/home_screen.dart
+++ lib/widgets/home_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+import 'package:cistella_cat/provider/general_provider.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    int counter = Provider.of<GeneralProvider>(context).counter;
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$counter',
+              style: const TextStyle(fontSize: 24),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Provider.of<GeneralProvider>(context, listen: false).increment();
+              },
+              child: const Text('Increment'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/my_app.dart
+++ lib/widgets/my_app.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+import 'package:cistella_cat/provider/general_provider.dart';
+
+import 'package:cistella_cat/widgets/home_screen.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => GeneralProvider()),
+      ],
+      child: const MaterialApp(
+        title: 'Cistella Cat',
+        home: HomeScreen(),
+      ),
+    );
   }
 }
